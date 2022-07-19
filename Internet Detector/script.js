6a4b96dbeef8404bff26645a3c2156efe0e3f93c
Satish Yadav <satish.yadav@nagarro.com>
2022-07-19 11:13:49 +0530

    check online status with navigor api

--- Internet Detector/script.js
+++ Internet Detector/script.js
@@ -1,44 +1,39 @@
-const wrapper = document.querySelector(".wrapper"),
-toast = wrapper.querySelector(".toast"),
-title = toast.querySelector("span"),
-subTitle = toast.querySelector("p"),
-wifiIcon = toast.querySelector(".icon"),
-closeIcon = toast.querySelector(".close-icon");
+const wrapper = document.querySelector('.wrapper'),
+	toast = wrapper.querySelector('.toast'),
+	title = toast.querySelector('span'),
+	subTitle = toast.querySelector('p'),
+	wifiIcon = toast.querySelector('.icon'),
+	closeIcon = toast.querySelector('.close-icon');
 
-window.onload = ()=>{
-    function ajax(){
-        let xhr = new XMLHttpRequest();
-        xhr.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
-        xhr.onload = ()=>{
-            if(xhr.status == 200 && xhr.status < 300){
-                toast.classList.remove("offline");
-                title.innerText = "You're online now";
-                subTitle.innerText = "Hurray! Internet is connected.";
-                wifiIcon.innerHTML = '<i class="uil uil-wifi"></i>';
-                closeIcon.onclick = ()=>{ 
-                    wrapper.classList.add("hide");
-                }
-                setTimeout(()=>{ 
-                    wrapper.classList.add("hide");
-                }, 5000);
-            }else{
-                offline(); 
-            }
-        }
-        xhr.onerror = ()=>{
-            offline();
-        }
-        xhr.send(); 
-    }
+function autoHideAlert() {
+	setTimeout(() => wrapper.classList.add('hide'), 2000);
+}
 
-    function offline(){ 
-        wrapper.classList.remove("hide");
-        toast.classList.add("offline");
-        title.innerText = "You're offline now";
-        subTitle.innerText = "Opps! Internet is disconnected.";
-        wifiIcon.innerHTML = '<i class="uil uil-wifi-slash"></i>';
-    }
-    setInterval(()=>{ 
-        ajax();
-    }, 100);
+window.addEventListener('load', () => {
+	const internetStatus = navigator.onLine;
+	console.log(internetStatus);
+	internetStatus ? onlineAlert() : offlineAlert;
+});
+
+window.addEventListener('online', () => onlineAlert());
+window.addEventListener('offline', () => offlineAlert());
+
+closeIcon.onclick = () => {
+	wrapper.classList.add('hide');
+};
+
+function onlineAlert() {
+	toast.classList.remove('offline');
+	title.innerText = "You're online now";
+	subTitle.innerText = 'Hurray! Internet is connected.';
+	wifiIcon.innerHTML = '<i class="uil uil-wifi"></i>';
+	autoHideAlert();
+}
+function offlineAlert() {
+	wrapper.classList.remove('hide');
+	toast.classList.add('offline');
+	title.innerText = "You're offline now";
+	subTitle.innerText = 'Opps! Internet is disconnected.';
+	wifiIcon.innerHTML = '<i class="uil uil-wifi-slash"></i>';
+	autoHideAlert();
 }
